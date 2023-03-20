aa821f5b32ce7de3d811d4ba8434cfe3b1357e0e
Syed Tahir Hussan <tahirsherazi786@gmail.com>
2023-03-20 22:33:45 +0500

    Add project files.

---
+++ STHNew/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace STHNew.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
